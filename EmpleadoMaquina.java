6c1e1621f14115ff24b93a1848d7eea1009f9cdb
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-05-06 08:20:43 +0200

    add clase EmpleadoMaquina
    Falta definir como resolver el metodo retirarTickets. Especificamente el a donde va lo retirado.

---
+++ EmpleadoMaquina.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class EmpleadoMaquina extends Empleado{
+
+    private String idEmpleadoMaquina;
+
+
+    public EmpleadoMaquina(String idUsuario, String nombre, String apellido, Date fecNacimiento,
+                           String contrasena, String correoElectronico, String idEmpleado, String puesto, String idEmpleadoMaquina) {
+        super(idUsuario, nombre, apellido, fecNacimiento, contrasena, correoElectronico, idEmpleado, puesto);
+        this.idEmpleadoMaquina = idEmpleadoMaquina;
+    }
+
+
+    public String getIdEmpleadoMaquina() {
+        return idEmpleadoMaquina;
+    }
+
+    public void setIdEmpleadoMaquina(String idEmpleadoMaquina) {
+        this.idEmpleadoMaquina = idEmpleadoMaquina;
+    }
+
+    public void encenderMaquina(Maquina maquina){
+        if(maquina.getHabilitada() || maquina.getDaniada()){
+        maquina.encender();}
+        else{
+            System.out.print("La maquina no está en condiciones de ser utilizada.");
+        }
+    }
+
+
+    public void apagarMaquina(Maquina maquina){
+        maquina.apagar();
+    }
+
+
+    public void retirarTickets(Maquina maquina, int cantidadTickets){
+        // FALTA DEFINIR SI LO RETIRADO VA A CAJA O A DONDE
+    }
+
+    public void recargarTickets(Maquina maquina, int cantidadTickets){
+
+        maquina.setSaldoTickets((maquina.getSaldoTickets() + cantidadTickets));
+    }
+}
