6e94c30bcdda50abb9b567e740a3cca058c82b17
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-05-06 14:07:38 -0300

    Update Caja.java
    Cambios caja

--- Caja.java
+++ Caja.java
@@ -2,24 +2,32 @@
 public class Caja {
 
     private int idCaja;
-
-    
+    private double saldoInicial;
     private double saldoActual;
     private EmpleadoCaja empleadoCaja;
 
-    public Caja(int idCaja, double saldoActual, EmpleadoCaja empleadoCaja) {
+    public Caja(int idCaja,double saldoInicial, double saldoActual, EmpleadoCaja empleadoCaja) {
         this.idCaja = idCaja;
+        this.saldoInicial = saldoInicial;
         this.saldoActual = saldoActual;
         this.empleadoCaja = empleadoCaja;
     }
-
-    public int getIdCaja() {
+    
+	public int getIdCaja() {
         return idCaja;
     }
 
     public void setIdCaja(int idCaja) {
         this.idCaja = idCaja;
     }
+    
+    public double getSaldoInicial() {
+		return saldoInicial;
+	}
+
+	public void setSaldoInicial(double saldoInicial) {
+		this.saldoInicial = saldoInicial;
+	}
 
     public double getSaldoActual() {
         return saldoActual;
