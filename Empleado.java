65384d35e5283f79f8561b3c1a292c818d9b2445
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-05-06 06:55:01 +0200

    add clase Empleado
    Se crea la clase Empleado

---
+++ Empleado.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+
+public class Empleado extends Usuario{
+
+    private int idEmpleado;
+
+    private String puesto;
+
+
+    public Empleado(String idUsuario, String nombre, String apellido, Date fecNacimiento, String contrasena, String correoElectronico, int idEmpleado, String puesto) {
+        super(idUsuario, nombre, apellido, fecNacimiento, contrasena, correoElectronico);
+        this.idEmpleado = idEmpleado;
+        this.puesto = puesto;
+    }
+
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+}
