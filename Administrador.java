4ae46a4f90e548802d64c882a1d68944f9b874da
MaMontealegre <matias.montealegre@davinci.edu.ar>
2023-05-06 20:35:31 -0300

    Clase Administrador y Main

--- Administrador.java
+++ Administrador.java
@@ -1,42 +1,43 @@
+package casino;
 
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Administrador extends Usuario{
+public class Administrador extends Usuario {
 
 	private int idAdministrador;
-	
-	
-	public Administrador(String usuarioId, String nombre, String apellido, Date fec_nacimiento, String contraseña, int idAdministrador, String correoElectronico) {
-		super(usuarioId, nombre, apellido, fec_nacimiento, contraseña, correoElectronico);
+
+	public Administrador(String idUsuario, String nombre, String apellido, Date fecNacimiento, String contraseña, String correoElectronico,
+			int idAdministrador) {
+		super(idUsuario, nombre, apellido, fecNacimiento, contraseña, correoElectronico);
 		this.idAdministrador = idAdministrador;
 	}
-		
-	// visualizar CAJA 
+
+	// visualizar CAJA
 	public void visualizarCaja(Caja caja) {
 		System.out.println("Saldo Inicial: " + caja.getSaldoInicial());
 		System.out.println("Saldo Actual: " + caja.getSaldoActual());
 	}
-	
-	
+
 	// eliminar y agregar JUEGO en maquina
 	public void agregarJuegoaMaquina(Juego juego, Maquina maquina) {
-		 maquina.setJuego(juego);
+		maquina.setJuego(juego);
 	}
-	
+
 	public void eliminarJuegoaMaquina(Maquina maquina) {
 		maquina.setJuego(null);
 	}
-	
+
 	// editar y eliminar Juego
-	
-	public void editarJuego(Juego juego, String nombre, String descripcion, int jugadoresMinimos, int jugadoresMaximos) {
+
+	public void editarJuego(Juego juego, String nombre, String descripcion, int jugadoresMinimos,
+			int jugadoresMaximos) {
 		juego.setNombre(nombre);
 		juego.setDescripcion(descripcion);
 		juego.setJugadoresMinimos(jugadoresMinimos);
 		juego.setJugadoresMaximos(jugadoresMaximos);
 	}
-	
+
 	public void eliminarJuego(Juego juego, ArrayList<Juego> juegoEjemplo) {
 		if (juegoEjemplo.contains(juego)) {
 			juegoEjemplo.remove(juego);
@@ -44,36 +45,33 @@ public class Administrador extends Usuario{
 		} else {
 			System.out.println("El juego " + juego.getNombre() + " no existe.");
 		}
-		
+
 	}
-	
-	
+
 	// Visualizar Cliente
-	
+
 	public void revisarCuentaCliente(Cliente cliente) {
 		System.out.println("Nombre : " + cliente.getNombre());
 		System.out.println("Apellido: " + cliente.getApellido());
-        System.out.println("Fecha de nacimiento: " + cliente.getFecNacimiento());
-        System.out.println("Correo Electronico: " + cliente.getCorreoElectronico());
-		
+		System.out.println("Fecha de nacimiento: " + cliente.getFecNacimiento());
+		System.out.println("Correo Electronico: " + cliente.getCorreoElectronico());
+
 	}
-	
-	
+
 	// Eliminar Usuario
-	
-	public void eliminarUsuario(Usuario usuario, ArrayList<Usuario> usuarioEjemplo ) {
+
+	public void eliminarUsuario(Usuario usuario, ArrayList<Usuario> usuarioEjemplo) {
 		if (usuarioEjemplo.contains(usuario)) {
 			usuarioEjemplo.remove(usuario);
 			System.out.println("El usuario " + usuario + " fue eliminado con éxito.");
 		} else {
 			System.out.println("El usuario " + usuario + " no existe.");
 		}
-		
+
 	}
-	
-	
-	//getters y setters
-	
+
+	// getters y setters
+
 	public int getIdAdministrador() {
 		return idAdministrador;
 	}
@@ -81,7 +79,5 @@ public class Administrador extends Usuario{
 	public void setIdAdministrador(int idAdministrador) {
 		this.idAdministrador = idAdministrador;
 	}
-	
-	 
 
 }
