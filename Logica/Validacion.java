b5f4e419b95f9a0fcae5fe2ddaff8f2c6646fe39
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-06-04 22:13:39 -0300

    Update Validacion.java

--- Logica/Validacion.java
+++ Logica/Validacion.java
@@ -5,16 +5,18 @@ import Datos.Administrador;
 
 public class Validacion {
 
-
-    public String validarIngreso(int id){
+    public String validarIngreso(int id, String contrasena){
 
         if(id<0){
             return "El id no puede ser menor a 0";
-        }
-        else{
-
-           return "Todo bien";
-        }
-
+            
+        }else {
+        	int tamanioContrasena=contrasena.length();
+        	if (tamanioContrasena >= 4 && tamanioContrasena <= 10){
+        		return "Todo bien";
+            } else {
+                	return "La contraseña debe tener entre 4 y 10 caracteres";
+            }
+       }
     }
 }
