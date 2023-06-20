2889273c7768d4ebc76dce5962717b8fbe56e4de
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2023-06-20 00:33:04 -0300

    Add files via upload

--- Datos/EmpleadoMaquina.java
+++ Datos/EmpleadoMaquina.java
@@ -5,7 +5,14 @@ import Logica.Validacion;
 
 import java.sql.SQLException;
 import java.util.Date;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,6 +20,10 @@ import java.sql.ResultSet;
 public class EmpleadoMaquina extends Empleado implements Menu{
 
     private int idEmpleadoMaquina;
+    private JFrame ventana;
+    private JButton botonEncender;
+    private JButton botonApagar;
+    private JPanel panel;
 
     public EmpleadoMaquina(int idUsuario, String nombre, String apellido, Date fecNacimiento, String contrasena, String direccion, String correoElectronico, int idEmpleado, String puesto) {
         super(idUsuario, nombre, apellido, fecNacimiento, contrasena, direccion, correoElectronico, idEmpleado, puesto);
@@ -108,47 +119,45 @@ public class EmpleadoMaquina extends Empleado implements Menu{
 
 
     public void mostrarMenu(String id){
-        String[] opcionesEMaquina = {"Encender Maquina", "Apagar Maquina", "Salir"};
+    	  String[] opcionesEMaquina = {"Encender Maquina", "Apagar Maquina", "Salir"};
 
-        String opcion;
+          ventana = new JFrame("Empleado Maquina");
+          ventana.setSize(300, 200);
+          ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          ventana.setLocationRelativeTo(null);
 
-        Validacion validacion = new Validacion();
-		do {
-			
-			opcion = (String) JOptionPane.showInputDialog(null, "Opciones Empleado Maquina", "Menu empleado maquina",
-					JOptionPane.DEFAULT_OPTION, null, opcionesEMaquina, opcionesEMaquina);
-			int idmaquina;
-			switch (opcion) {
-			
-			case "Encender Maquina":
-				idmaquina = Integer.parseInt( JOptionPane.showInputDialog("Ingrese el ID de la Maquina a encender"));
+          botonEncender = new JButton("Encender Maquina");
+          botonApagar = new JButton("Apagar Maquina");
 
-				if(validacion.validarExistenciaMaquina(idmaquina)){
-					this.encenderMaquina(idmaquina);
-					JOptionPane.showMessageDialog(null,"La maquina numero "+idmaquina+" ha sido encendida exitosamente",
-							"Encendido exitoso",JOptionPane.INFORMATION_MESSAGE);
-				}
-				break;
-				
-			case "Apagar Maquina":
-				idmaquina = Integer.parseInt( JOptionPane.showInputDialog("Ingrese el ID de la Maquina a apagar"));
+          panel = new JPanel();
+          panel.add(botonEncender);
+          panel.add(botonApagar);
 
-				if(validacion.validarExistenciaMaquina(idmaquina)){
-					this.apagarMaquina(idmaquina);
-					JOptionPane.showMessageDialog(null,"La maquina numero "+idmaquina+" ha sido apagada exitosamente",
-							"Apagado exitoso",JOptionPane.INFORMATION_MESSAGE);
-				}
+          ventana.add(panel);
+          ventana.setVisible(true);
 
-				break;
-				
-			case "Salir":
-				
-				break;
-			default:
-				break;
-			}
-		
-		} while(!opcion.equals("Salir"));
-    
+          Validacion validacion = new Validacion();
+
+          botonEncender.addActionListener(new ActionListener() {
+              public void actionPerformed(ActionEvent e) {
+                  int idMaquina = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID de la Maquina a encender"));
+                  if (validacion.validarExistenciaMaquina(idMaquina)) {
+                      encenderMaquina(idMaquina);
+                      JOptionPane.showMessageDialog(null, "La maquina numero " + idMaquina +
+                              " ha sido encendida exitosamente", "Encendido exitoso", JOptionPane.INFORMATION_MESSAGE);
+                  }
+              }
+          });
+
+          botonApagar.addActionListener(new ActionListener() {
+              public void actionPerformed(ActionEvent e) {
+                  int idMaquina = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID de la Maquina a apagar"));
+                  if (validacion.validarExistenciaMaquina(idMaquina)) {
+                      apagarMaquina(idMaquina);
+                      JOptionPane.showMessageDialog(null, "La maquina numero " + idMaquina +
+                              " ha sido apagada exitosamente", "Apagado exitoso", JOptionPane.INFORMATION_MESSAGE);
+                  }
+              }
+          });
     }
 }
