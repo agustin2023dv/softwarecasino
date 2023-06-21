d2663235d698e434117bd9af241225166a3e8a50
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-06-22 00:16:02 +0200

    JFRAME x JOPTIONPANE en validaciones

--- Logica/Validacion.java
+++ Logica/Validacion.java
@@ -4,8 +4,21 @@ package Logica;
 import Datos.*;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Validacion {
+
+	private void mostrarError(String mensaje) {
+		JFrame errorFrame = new JFrame("Error");
+		errorFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		errorFrame.setSize(300, 100);
+		errorFrame.setLayout(new FlowLayout());
+
+		JLabel labelError = new JLabel(mensaje);
+		errorFrame.add(labelError);
+
+		errorFrame.setVisible(true);
+	}
     
 //VALIDACIONES LOGIN
 
@@ -16,13 +29,13 @@ public class Validacion {
 		int longitudID= idUsuario.length();
 
 		if (longitudID < 0 || longitudID > 99 || idUsuario.isEmpty()) {
-			JOptionPane.showMessageDialog(null,"El usuario debe ser un numero entre 1 y 99","Error",
-					JOptionPane.ERROR_MESSAGE);
+
+
+			mostrarError("El usuario debe ser un numero entre 1 y 99");
 			return false; // El ID de usuario debe estar entre 0 y 99
 		}
 		else if(longitudContrasena < 6 || longitudContrasena > 15){
-			JOptionPane.showMessageDialog(null,"La contraseña debe tener entre 6 y 15 caracteres","Error",
-					JOptionPane.ERROR_MESSAGE);
+			mostrarError("La contraseña debe tener entre 6 y 15 caracteres");
 			return false;
 		}
 		else {
@@ -74,9 +87,7 @@ public class Validacion {
 	public boolean validarVerCaja(int idCaja){
 
 			if(idCaja<=0 || idCaja>4){
-				JOptionPane.showMessageDialog(null,
-						"La caja " + idCaja + " no existe o no tiene transacciones.",
-						"Error", JOptionPane.ERROR_MESSAGE);
+				mostrarError("La caja " + idCaja + " no existe o no tiene transacciones.");
 				return false;
 			}
 			else{
@@ -89,12 +100,10 @@ public class Validacion {
 
 
 		if(jugadoresMinimos < 1 || jugadoresMaximos > 6) {
-			JOptionPane.showMessageDialog(null,"La cantidad de jugadores mínimos debe ser mayor o igual a 1 y menor o igual a 6"
-					,"Error",JOptionPane.ERROR_MESSAGE);
+			mostrarError("La cantidad de jugadores mínimos debe ser mayor o igual a 1 y menor o igual a 6");
             return false;
 		}else if (descripcion.length() < 10 || descripcion.length() > 100){
-			JOptionPane.showMessageDialog(null,"La descripcion debe tener entre 10 y 100 caracteres",
-					"Error",JOptionPane.ERROR_MESSAGE);
+			mostrarError("La descripcion debe tener entre 10 y 100 caracteres");
 			 return false;
 		}else {
 			return true;
@@ -133,8 +142,8 @@ public class Validacion {
 	public boolean validarExistenciaCliente(int idUsuario) {
 
 		if(idUsuario <= 0 || idUsuario >99 ){
-			JOptionPane.showMessageDialog(null,
-					"El ID del Usuario debe ser un numero entre 1 y 99", "Error", JOptionPane.ERROR_MESSAGE);
+
+			mostrarError("El ID del Usuario debe ser un numero entre 1 y 99");
 			return false;
 		}
 		else{
